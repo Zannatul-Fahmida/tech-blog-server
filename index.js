8819b1d6f750af30256b556e81a4da36a293375d
zannatul <jannatulfahmida15@gmail.com>
2022-12-09 19:53:30 +0600

    single blog api added

--- index.js
+++ index.js
@@ -29,6 +29,14 @@ const run = async () => {
       res.send({ status: true, data: blog });
     });
 
+    // get single blog
+    app.get("/blog/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const blog = await blogsCollection.findOne(query);
+      res.send({ status: true, data: blog });
+    });
+
     // post new blog
     app.post("/blog", async (req, res) => {
       const blog = req.body;
